89ac78a4462fbac858cd478af061e46128f6756e
nkc-Fujishima <muzingattai@icloud.com>
2024-09-17 09:57:09 +0900

    Update : ダメージインターフェースにオーバーロードを追加

--- ShikakuProject/Assets/Public/Script/Interface/IDamage.cs
+++ ShikakuProject/Assets/Public/Script/Interface/IDamage.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 
-public interface IDamage 
+public interface IDamage
 {
-    public void Damage() { Debug.Log("IDamage : Damage　デフォルト実装が起動"); }
+    public void Damage() { Debug.Log("IDamage : Damage デフォルト実装が起動"); }
+
+    public void Damage(Vector3 position) { Debug.Log("IDamage(Vector3 position) : Damage デフォルト実装が起動"); }
 }
