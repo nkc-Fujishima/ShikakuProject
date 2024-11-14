b90545138d42478977d0a0e7b8ccd98bffc32395
Haruki-Yagezawa <97860607+Haruki-Yagezawa@users.noreply.github.com>
2024-11-14 21:20:42 +0900

    add:チュートリアルのオブジェクトを追加

---
+++ ShikakuProject/Assets/Tutorial/Sctipt/Controller/TutorialCharactorController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class TutorialCharactorController : MonoBehaviour
+{
+    [SerializeField]
+    private UITalkManager _talkManager;
+
+    [SerializeField]
+    private Animator _animator;
+
+    [SerializeField]
+    private AudioSource _audioSource;
+
+    [SerializeField]
+    private AudioClip _audioClip;
+
+    [SerializeField]
+    private Rigidbody _rigidbodyChara;
+
+    [SerializeField]
+    private float _sensingDistance = 10f;
+
+    [Header("˜b‚·“à—e")]
+    [SerializeField]
+    [Multiline(3)]
+    private string text;
+
+    private Transform _transformChara;
+
+    private bool _isTalking = false;
+
+
+    private void Start()
+    {
+        _talkManager.OnStart(text);
+
+        _transformChara = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        if ((_transformChara.position - transform.position).magnitude > _sensingDistance)
+        {
+            if (!_isTalking) return;
+
+            OnEnd();
+            _isTalking = false;
+
+            return;
+        }
+
+        if (_isTalking) return;
+
+        OnActive();
+        _isTalking = true;
+    }
+
+    private void OnActive()
+    {
+        if (_audioClip && _audioSource)
+            _audioSource.PlayOneShot(_audioClip);
+
+        if (_animator)
+            _animator.SetTrigger("jump");
+
+        _rigidbodyChara.AddForce(0, 200, 0);
+
+        _talkManager.ActiveTalk();
+    }
+
+    private void OnEnd()
+    {
+        _talkManager.EndTalk();
+    }
+}
---
+++ ShikakuProject/Assets/UI/Scripts/TalkUI/UITalkManager.cs
@@ -0,0 +1,81 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITalkManager : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform _imageTalk;
+
+    [SerializeField]
+    private Text _textTalk;
+
+    [SerializeField]
+    private Transform _transformTalk;
+
+    [SerializeField]
+    private CanvasGroup _canvasGroup;
+
+
+    private readonly float _talkUIOutlineThickness = 20;
+
+
+    private readonly float _duration = 0.2f;
+
+    private Vector2 _activePoint = Vector2.zero;
+
+    private Vector2 _endPoint = new(0, 0.3f);
+
+    public void OnStart(string text)
+    {
+        SetBackImageScale(in text);
+
+        _textTalk.text = text;
+
+        transform.rotation = Camera.main.transform.rotation;
+    }
+
+    // Œã‚ë‚Ì‰æ‘œ‚Ì‘å‚«‚³‚ð•¶Žš”‚É‡‚¤‚æ‚¤‚É’²ß
+    private void SetBackImageScale(in string text)
+    {
+        Vector2 textImageLange = new();
+        int paragraphCount = text.Split('\n').Length;
+        textImageLange.y = paragraphCount * _textTalk.fontSize + _talkUIOutlineThickness * 2;
+
+        int characterCount = CheckLengthLongestString(in text);
+        textImageLange.x = characterCount * _textTalk.fontSize + _talkUIOutlineThickness * 2;
+
+        _imageTalk.sizeDelta = textImageLange;
+    }
+
+    // Žw’è‚µ‚½•¶Žš—ñ‚ª‚¢‚­‚Â‚ ‚é‚©
+    private int CheckLengthLongestString(in string text)
+    {
+        string[] paragraphs = text.Split('\n');
+        int maxLength = 0;
+        foreach (string paragraph in paragraphs)
+        {
+            if (paragraph.Length > maxLength)
+            {
+                maxLength = paragraph.Length;
+            }
+        }
+
+        return maxLength;
+    }
+
+
+    public void ActiveTalk()
+    {
+        _canvasGroup.DOFade(1, _duration);
+
+        _transformTalk.DOLocalMove(_activePoint, _duration);
+    }
+
+    public void EndTalk()
+    {
+        _canvasGroup.DOFade(0, _duration);
+
+        _transformTalk.DOLocalMove(_endPoint, _duration);
+    }
+}
